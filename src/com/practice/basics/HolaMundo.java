a64bce29608e6478e9358aba0f22415ab5ac942e
Jefferson Ayovi <105981230+jeffayovi@users.noreply.github.com>
2025-11-05 10:25:37 -0500

    feat: agregar programa Hola Mundo

---
+++ src/com/practice/basics/HolaMundo.java
@@ -0,0 +1,8 @@
+package com.practice.basics;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola,Mundo!");
+        System.out.println("Mi primer pograma en Java");
+    }
+}
