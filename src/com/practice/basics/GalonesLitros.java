0d64caa44f1099044d8fca5e649d277b2321b7b7
Jefferson Ayovi <105981230+jeffayovi@users.noreply.github.com>
2025-11-06 16:43:23 -0500

    feat: agregar programa convierte galones a litros.

---
+++ src/com/practice/basics/GalonesLitros.java
@@ -0,0 +1,17 @@
+package com.practice.basics;
+//Try This 1-1
+// Este programa convierte galones a litros.
+public class GalonesLitros {
+    public static void main(String[] args) {
+
+        double galones;
+        double litros;
+
+        galones = 10;
+
+        litros = galones * 3.7854;
+
+        System.out.println(galones + " galones es "+ litros + " litros");
+
+    }
+}
